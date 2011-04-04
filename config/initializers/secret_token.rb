39ad598ab056bcb890191651cec283aa05efe10c
Nicolas <nicolas.papon@webflows.fr>
2011-04-04 14:53:35 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Meublespecoste::Application.config.secret_token = '2d10678a5efb998d8ff1742d1e8681b87f5cfdefdf6f3f93ab010f79f2ec915009839d1a411b89d68d68227c56d4ff62f1d9e16acafeab87c60cc19f6cdd3faa'
