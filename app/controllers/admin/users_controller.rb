50356bcc555e70ffdc038e5eaaa52c04f6b352a4
Nicolas <nicolas.papon@webflows.fr>
2011-04-20 13:55:54 +0200

    ajout du password dans l'admin des users

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,7 +1,8 @@
 class Admin::UsersController < Admin::ApplicationController
 
   active_scaffold :users do |config|
-    config.columns= [:email, :created_at]
+    config.list.columns= [:email, :created_at]
+    config.create.columns= config.update.columns=[:email, :password]
     config.actions.exclude :show
   end
 
